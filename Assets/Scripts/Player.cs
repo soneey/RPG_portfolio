2bf7ec62a71ea32ed15d4a8be1f64005db8a1b21
soneey <slainson@naver.com>
2023-12-21 19:51:35 +0900

    12/21

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,61 +2,78 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    [SerializeField] private float moveDelay = 1.0f;
-    [SerializeField] private float moveSpeed = 5.0f;
+    [SerializeField] private float moveDelay = 0.5f;
     private float moveDelayTimer = 0.0f;
     private bool checkMoving;
 
     Rigidbody rigid;
-
+    Animator animator;
+    SpriteRenderer spriteRenderer;
+    Sprite sprite;
     Vector3 moveVec;
 
     private void Awake()
     {
         rigid = GetComponent<Rigidbody>();
+        sprite = GetComponent<Sprite>();
     }
     void Start()
     {
-
+        animator = transform.GetComponent<Animator>();
     }
 
     void Update()
     {
         moving();
+        turning();
     }
 
     private void moving()
     {
+        if (Input.GetKey(KeyCode.RightArrow) && checkMoving == false)
+        {
+            moveVec = transform.position;
+            moveVec += new Vector3(1, 0, 0);
+            transform.position = moveVec;
+
+
+            
+            checkMoving = true;
+        }
+        if (Input.GetKey(KeyCode.LeftArrow) && checkMoving == false)
+        {
+            moveVec = transform.position;
+            moveVec += new Vector3(-1, 0, 0);
+            transform.position = moveVec;
+            checkMoving = true;
+        }
+        if (Input.GetKey(KeyCode.UpArrow) && checkMoving == false)
+        {
+            moveVec = transform.position;
+            moveVec += new Vector3(0, 1, 0);
+            transform.position = moveVec;
+
+            animator.SetInteger("WalkBack", 1);
+
+            checkMoving = true;
+        }
+        if (Input.GetKey(KeyCode.DownArrow) && checkMoving == false)
+        {
+            moveVec = transform.position;
+            moveVec += new Vector3(0, -1, 0);
+            transform.position = moveVec;
+            checkMoving = true;
+        }
         checkDelay();
-        if (Input.GetKey(KeyCode.RightArrow))
+    }
+    private void turning()
+    {
+        if (Input.GetKey(KeyCode.LeftShift) && Input.GetKey(KeyCode.RightArrow))
         {
-            moveVec = transform.position;
-            moveVec += new Vector3(1, 0, 0) * Time.deltaTime;
-            transform.position = moveVec;
-            checkMoving = true;
-        }
-        if (Input.GetKey(KeyCode.LeftArrow))
-        {
-            moveVec = transform.position;
-            moveVec += new Vector3(-1, 0, 0) * Time.deltaTime;
-            transform.position = moveVec;
-            checkMoving = true;
-        }
-        if (Input.GetKey(KeyCode.UpArrow))
-        {
-            moveVec = transform.position;
-            moveVec += new Vector3(0, 1, 0) * Time.deltaTime;
-            transform.position = moveVec;
-            checkMoving = true;
-        }
-        if (Input.GetKey(KeyCode.DownArrow))
-        {
-            moveVec = transform.position;
-            moveVec += new Vector3(0, -1, 0) * Time.deltaTime;
-            transform.position = moveVec;
-            checkMoving = true;
+            sprite = spriteRenderer.sprite; 
         }
     }
+
     private void checkDelay()
     {
         if (checkMoving == false) { return; }
